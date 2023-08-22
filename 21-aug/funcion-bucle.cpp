c59ca686f251d4efc71be0ffd3fdd38a776fe0ce
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2023-08-22 09:56:49 -0600

    Ejercicio potencia

--- 21-aug/funcion-bucle.cpp
+++ 21-aug/funcion-bucle.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
+int Exponentation(int base, int exponent);
+
 int main()
 {
+  cout << Exponentation(2, 3);
+
   return 0;
+}
+
+int Exponentation(int base, int exponent)
+{
+  int result = 1;
+
+  for (int i = 0; i < exponent; i++)
+  {
+    result *= base;
+  }
+
+  return result;
 }
