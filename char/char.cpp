2e614841b25d541bc14006bef062cb00b77dabf8
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2023-10-09 11:34:01 -0600

    Clase char

---
+++ char/char.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  // char variable = 'A';
+  // string dato = "nombre";
+  // char charvariable[] = "nombre";
+  // char charvarible1[6] = {'n', 'o', 'm', 'b', 'r', 'e'};
+
+  // cout << dato << endl;
+  // cout << charvariable << endl;
+
+  // char letra = 'M';
+  // cout << letra << endl;
+  // cout << (int)letra << endl;
+
+  int letra = 97;
+  cout << letra << endl;
+  cout << (char)letra << endl;
+
+  return 0;
+}
