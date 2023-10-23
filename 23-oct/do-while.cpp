8185dfaafad89466c58214363ce8c2066ca6489f
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2023-10-23 12:07:04 -0600

    Do while modificado

--- 23-oct/do-while.cpp
+++ 23-oct/do-while.cpp
@@ -5,5 +5,19 @@ using namespace std;
 int main()
 {
 
+  string respuesta = "s";
+
+  while (respuesta == "s")
+  {
+    cout << "te quiero mucho uwu ";
+    // cin >> respuesta;
+  }
+
+  do
+  {
+    cout << "Seguir repitiendo 2? ";
+    cin >> respuesta;
+  } while (respuesta == "s");
+
   return 0;
 }
